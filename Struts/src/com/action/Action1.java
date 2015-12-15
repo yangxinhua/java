a22a67189cb43a8da0892dc67873f811b5b6d831
yangxinhua <jackyhua@vip.qq.com>
2015-12-15 21:47:56 +0800

    action

--- Struts/src/com/action/Action1.java
+++ Struts/src/com/action/Action1.java
@@ -2,7 +2,7 @@ package com.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class Action extends ActionSupport {
+public class Action1 extends ActionSupport {
 	public String execute()
 	{
 		return "success";
