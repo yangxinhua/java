8d52c1eb8b80ec03a36f68e43535d6c01a71f48a
yangxinhua <jackyhua@vip.qq.com>
2016-01-07 14:10:49 +0800

    idea maven struts demo

---
+++ demo/src/main/java/com/action/ActionDemo.java
@@ -0,0 +1,13 @@
+package com.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by jacky on 2016/1/6.
+ */
+public class ActionDemo extends ActionSupport {
+    public String execute()
+    {
+        return "success";
+    }
+}
