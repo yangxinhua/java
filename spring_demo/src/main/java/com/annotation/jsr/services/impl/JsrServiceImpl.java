5b109a3350f792f51450ec1864d4be3d923b91e1
yangxinhua <jackyhua@vip.qq.com>
2016-01-27 10:57:46 +0800

    注解结束

--- spring_demo/src/main/java/com/annotation/jsr/services/impl/JsrServiceImpl.java
+++ spring_demo/src/main/java/com/annotation/jsr/services/impl/JsrServiceImpl.java
@@ -7,17 +7,21 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 /**
  * Created by jacky on 2016/1/26.
  */
-@Service
+//@Service
+    @Named
 public class JsrServiceImpl implements IJsrService{
     //resource注解可以直接放在成员前，或者通过set方法实现注入。效果等同。
-    @Resource
-    public void setJsrdaoimpl(JsrDAOImpl jsrdaoimpl) {
+//    @Resource
+    @Inject
+    public void setJsrdaoimpl(@Named("jsrDAOImpl")JsrDAOImpl jsrdaoimpl) {
         this.jsrdaoimpl = jsrdaoimpl;
-    }
+    }//通过named指定bean对应实例名称，解决类似于一个接口对应多个实现的问题。
 
     //    @Resource
     private JsrDAOImpl jsrdaoimpl;
